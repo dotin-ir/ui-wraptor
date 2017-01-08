c7c893ca19d9c91a802b8569e84c556dd702b13e
e.khodadadi <e.khodadadi@dotin.ir>
2017-01-08 16:51:02 +0330

    RPTR-344 ‫تصحیح پیاده‌سازی popup با استفاده از Redux

---
+++ src/Popup/Popup.jsx
@@ -0,0 +1,57 @@
+import React, {PropTypes} from "react";
+import BaseComponent from "../BaseComponent";
+import Dialog from "dotin-material-ui/Dialog";
+import FlatButton from 'dotin-material-ui/FlatButton';
+
+class Popup extends BaseComponent {
+
+    static propTypes = {
+        title: PropTypes.string,
+        closeCaption: PropTypes.string.isRequired,
+        onBeforeClose: PropTypes.func,
+        onClosing: PropTypes.func,
+        onAfterClosed: PropTypes.func.isRequired,
+        overlayVisible: PropTypes.bool
+    };
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            open: true,
+        };
+    }
+
+    handleClose() {
+        if (this.props.onBeforeClose && this.props.onBeforeClose() === false) {
+            return;
+        }
+        if (this.props.onClosing) {
+            this.props.onClosing();
+        }
+        this.setState({open: false});
+        this.props.onAfterClosed();
+    }
+
+    render() {
+        const overlayStyle = this.props.overlayVisible ? {} : {
+                backgroundColor: 'transparent'
+            };
+        return (
+            <Dialog
+                title={this.props.title}
+                titleStyle={{fontSize: 18, lineHeight: '10px'}}
+                actions={<FlatButton label={this.props.closeCaption} secondary={true}
+                                     onTouchTap={this.handleClose.bind(this)}/>}
+                modal={true}
+                open={this.state.open}
+                autoScrollBodyContent={true}
+                contentStyle={{width: '90%', height: '100%', maxWidth: 'none', maxHeight: 'none'}}
+                overlayStyle={overlayStyle}
+            >
+                {this.props.children}
+            </Dialog>
+        );
+    }
+}
+
+export default Popup;
